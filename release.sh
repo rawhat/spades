f2d3a4b852f3c7eaf071ea46ee54c9f0ef33e771
Alex Manning <alex41290@gmail.com>
2020-04-29 20:52:40 +0000

    secret key support in release script

--- release.sh
+++ release.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-set -eu
+stty -echo
+printf "DB Password: "
+read DB_PASS
+stty echo
+printf "\n"
 
-SECRET_KEY_BASE=$(mix phx.gen.secret)
+if [ -z $SECRET_KEY ]; then
+  SECRET_KEY_BASE="$(mix phx.gen.secret)"
+else
+  SECRET_KEY_BASE="${SECRET_KEY}"
+fi
+
 DATABASE_URL="ecto://${DB_USER}:${DB_PASS}@${DB_HOST}/spades"
 
 docker build -t spades -f Dockerfile.release ./
