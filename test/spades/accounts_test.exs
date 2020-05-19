e4426ddea939eb6b8c2630f4606363edccf7903b
Alex Manning <alex41290@gmail.com>
2020-05-18 20:08:24 -0400

    fix accounts test

--- test/spades/accounts_test.exs
+++ test/spades/accounts_test.exs
@@ -21,12 +21,12 @@ defmodule Spades.AccountsTest do
 
     test "list_users/0 returns all users" do
       user = user_fixture()
-      assert Accounts.list_users() == [user]
+      assert Accounts.list_users() == [%{user | password: nil}]
     end
 
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
-      assert Accounts.get_user!(user.id) == user
+      assert Accounts.get_user!(user.id) == %{user | password: nil}
     end
 
     test "create_user/1 with valid data creates a user" do
@@ -49,7 +49,7 @@ defmodule Spades.AccountsTest do
     test "update_user/2 with invalid data returns error changeset" do
       user = user_fixture()
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
-      assert user == Accounts.get_user!(user.id)
+      assert %{user | password: nil} == Accounts.get_user!(user.id)
     end
 
     test "delete_user/1 deletes the user" do
