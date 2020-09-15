9523954be8ba72f7e654c5f9c4016f7151599bc0
Alex Manning <alex41290@gmail.com>
2020-09-15 17:19:41 -0400

    run prettier

--- assets/src/EventStream.tsx
+++ assets/src/EventStream.tsx
@@ -3,10 +3,10 @@ import { useSelector } from "react-redux";
 
 import { selectEvents, selectPlayersById } from "./features/game/gameSlice";
 
-import { Bold } from "./Text"
+import { Bold } from "./Text";
 import { Container, PaddedVerticalLayout } from "./Layout";
 import { useDelayedRemove } from "./useDelayedRemove";
-import { cardToString } from "./Card"
+import { cardToString } from "./Card";
 
 function EventStream() {
   const events = useSelector(selectEvents);
@@ -19,56 +19,54 @@ function EventStream() {
       <PaddedVerticalLayout padding={2}>
         {eventStream.map((event, index) => {
           const eventText = `${index}-${JSON.stringify(event)}`;
-          switch(event.type) {
+          switch (event.type) {
             case "called": {
               return (
                 <GameEvent key={eventText}>
-                  {playersById[event.data.player]?.name || event.data.player} called {event.data.call}
+                  {playersById[event.data.player]?.name || event.data.player}{" "}
+                  called {event.data.call}
                 </GameEvent>
-              )
+              );
             }
             case "hand_ended": {
-              return (
-                <GameEvent key={eventText}>Hand ended</GameEvent>
-              )
+              return <GameEvent key={eventText}>Hand ended</GameEvent>;
             }
             case "dealt_cards": {
-              return (
-                <GameEvent key={eventText}>Dealt cards</GameEvent>
-              )
+              return <GameEvent key={eventText}>Dealt cards</GameEvent>;
             }
             case "played_card": {
               return (
                 <GameEvent key={eventText}>
-                  {playersById[event.data.player]?.name || event.data.player} played {cardToString(event.data.card)}
+                  {playersById[event.data.player]?.name || event.data.player}{" "}
+                  played {cardToString(event.data.card)}
                 </GameEvent>
-              )
+              );
             }
             case "round_ended": {
-              return (
-                <GameEvent key={eventText}>Round ended</GameEvent>
-              )
+              return <GameEvent key={eventText}>Round ended</GameEvent>;
             }
             case "awarded_trick": {
               return (
                 <GameEvent key={eventText}>
-                  Awarded trick to {playersById[event.data.winner]?.name || event.data.winner}
+                  Awarded trick to{" "}
+                  {playersById[event.data.winner]?.name || event.data.winner}
                 </GameEvent>
-              )
+              );
             }
             case "state_changed": {
               return (
                 <GameEvent key={eventText}>
                   Game changed from {event.data.old} to {event.data.new}
                 </GameEvent>
-              )
+              );
             }
             case "revealed_cards": {
               return (
                 <GameEvent key={eventText}>
-                  {playersById[event.data.player]?.name || event.data.player} revealed their hand
+                  {playersById[event.data.player]?.name || event.data.player}{" "}
+                  revealed their hand
                 </GameEvent>
-              )
+              );
             }
             default: {
               throw new Error("Unexpected event");
@@ -77,7 +75,7 @@ function EventStream() {
         })}
       </PaddedVerticalLayout>
     </PaddedVerticalLayout>
-  )
+  );
 }
 
 export default EventStream;
@@ -86,14 +84,10 @@ interface GameEventProps {
   children: React.ReactNode;
 }
 
-function GameEvent({children}: GameEventProps) {
+function GameEvent({ children }: GameEventProps) {
   return (
-    <Container
-      borderRadius={3}
-      border="1px solid lightgray"
-      padding="2 4"
-    >
+    <Container borderRadius={3} border="1px solid lightgray" padding="2 4">
       {children}
     </Container>
-  )
+  );
 }
